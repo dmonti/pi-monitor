948a79fac24fe5f4eda2e9d4c30f141244ad339d
Daniel Monti <dmonti@users.noreply.github.com>
2025-05-16 14:05:59 -0300

    fix: code refactor

--- index.ts
+++ index.ts
@@ -3,13 +3,12 @@ var t0 = Date.now()
 import pkg from './package.json' assert { type: 'json' }
 console.log(`Starting ${pkg.name} v${pkg.version} using Bun v${Bun.version} with PID ${process.pid}`)
 
-const port = process.env.SERVER_PORT || 3000
 const server = Bun.serve({
-  port: port,
-  fetch(req) {
+  port: process.env.SERVER_PORT || 3000,
+  async fetch(req) {
     return new Response("Bun!");
   },
 })
 
-console.log(`Bun server initialized with port ${port} (http)`)
+console.log(`Bun server initialized with port ${server.port} (http)`)
 console.log(`Started ${pkg.name} in ${Date.now() - t0}ms`)
