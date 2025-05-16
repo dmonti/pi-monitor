578c0381947f87e9df74de75c6b0a639aaf94adc
Daniel Monti <dmonti@users.noreply.github.com>
2025-05-16 12:39:06 -0300

    fix: set default port value

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ var t0 = Date.now()
 import pkg from './package.json' assert { type: 'json' }
 console.log(`Starting ${pkg.name} v${pkg.version} using Bun v${Bun.version} with PID ${process.pid}`)
 
-const port = process.env.SERVER_PORT
+const port = process.env.SERVER_PORT || 3000
 const server = Bun.serve({
   port: port,
   fetch(req) {
