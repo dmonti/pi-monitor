a76c237c822c2d222fc684db6f2db7bdb2d07d93
Daniel Monti <dmonti@users.noreply.github.com>
2025-05-16 12:36:15 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+var t0 = Date.now()
+
+import pkg from './package.json' assert { type: 'json' }
+console.log(`Starting ${pkg.name} v${pkg.version} using Bun v${Bun.version} with PID ${process.pid}`)
+
+const port = process.env.SERVER_PORT
+const server = Bun.serve({
+  port: port,
+  fetch(req) {
+    return new Response("Bun!");
+  },
+})
+
+console.log(`Bun server initialized with port ${port} (http)`)
+console.log(`Started ${pkg.name} in ${Date.now() - t0}ms`)
